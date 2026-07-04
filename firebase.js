054aab441715597be2280f22191d8822be8548e7
dobi10 <dozohi1029@gmail.com>
2026-07-05 00:56:27 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnEapm8W1ilLQiMImyHSLEGcTYDw8mKKk",
+  authDomain: "freedom-habesha.firebaseapp.com",
+  projectId: "freedom-habesha",
+  storageBucket: "freedom-habesha.firebasestorage.app",
+  messagingSenderId: "401375892502",
+  appId: "1:401375892502:web:6ac1591e7814686c91d137"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
